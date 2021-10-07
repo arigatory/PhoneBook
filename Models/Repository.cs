3f6cafeb8ff4e4e29f6e09652d11af1598e1c47a
Ivan MSI <arigatory@gmail.com>
2021-10-08 00:26:07 +0300

    done

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -6,7 +6,39 @@ namespace PhoneBook.Models
     public static class Repository
     {
         static int id = 0;
-        private static List<Contact> _contacts = new List<Contact>();
+        private static List<Contact> _contacts = new List<Contact>
+        {
+            new Contact
+            {
+                Id = NextId(),
+                LastName =  "Иванов",
+                FirstName = "Иван",
+                MiddleName = "Иванович",
+                Description = "мой самый первый контакт",
+                Address = "Москва, ул. Центральная, д. 5, кв. 37",
+                Phone = "+7(900)100-20-30" 
+            },
+            new Contact
+            {
+                Id = NextId(),
+                LastName =  "Петров",
+                FirstName = "Андрй",
+                MiddleName = "Петрович",
+                Description = "хороший друг",
+                Address = "Москва, ул. Ленина, д. 6, кв. 37",
+                Phone = "+7(900)100-20-30"
+            },
+            new Contact
+            {
+                Id = NextId(),
+                LastName =  "Сидоров",
+                FirstName = "Илья",
+                MiddleName = "Олегович",
+                Description = "бывший коллега",
+                Address = "Москва, Мичуринский пр-т, д. 5, кв. 100",
+                Phone = "+7(900)100-20-30"
+            }
+        };
         
         public static IEnumerable<Contact> Contacts => _contacts;
 
@@ -14,8 +46,7 @@ namespace PhoneBook.Models
         {
             if (contact.Id == 0)
             {
-                id++;
-                contact.Id = id;
+                contact.Id = NextId();
             }
             _contacts.Add(contact);
         }
@@ -26,6 +57,12 @@ namespace PhoneBook.Models
             {
                 _contacts.Remove(cnt);
             }
+        }
+
+        private static int NextId()
+        {
+            id++;
+            return id;
         }
     }
 }
