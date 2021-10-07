29d7991f9d8c0ec0f3cc41046be974d2e384d58c
Ivan MSI <arigatory@gmail.com>
2021-10-07 22:53:32 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ContactForm(Contact contact)
+        {
+            //TODO store responce 
+            return View();
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneBook.Models
+{
+    public static class Repository
+    {
+        private static List<Contact> _contacts = new List<Contact>();
+        
+        public static IEnumerable<Contact> Contacts => _contacts;
+
+        public static void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+    }
+}
