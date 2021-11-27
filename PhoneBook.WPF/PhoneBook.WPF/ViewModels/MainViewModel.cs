0ad716ccec1da508455bb81ff49ca0adba3e8b3c
Ivan MSI <arigatory@gmail.com>
2021-11-27 13:36:06 +0300

    changed folders

---
+++ PhoneBook.WPF/PhoneBook.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using PhoneBook.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhoneBook.WPF.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Contact _selectedContact;
+
+        public Contact SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                if (_selectedContact != value)
+                {
+                    _selectedContact = value;
+                    RaisePropertyChanged();
+                }
+
+            }
+        }
+
+        public ObservableCollection<Contact> Contacts { get; } = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
